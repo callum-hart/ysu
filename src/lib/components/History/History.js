3093250d8d33e2464586520bfcab7c6575e4df03
Callum Hart <callum@callumhart.com>
2020-05-24 17:48:59 +0100

    Drag & resize history

--- src/lib/components/History/History.js
+++ src/lib/components/History/History.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import cx from "classnames";
+import { Rnd } from "react-rnd";
 import Switch from "react-switch";
 
 import styles from "./History.module.css";
@@ -50,61 +51,73 @@ function History({ sequenceId, history, timeTravel }) {
   }, [history.length]);
 
   return (
-    <section className={cx(styles.history, styles[`history--${theme}`])}>
-      <div className={styles.header}>
-        <p className={styles.header__title}>{sequenceId}</p>
-        <label className={styles.switch}>
-          <>
-            {theme === "dark" && (
-              <span className={styles.switch__label}>Light</span>
-            )}
-            {theme === "light" && (
-              <span className={styles.switch__label}>Dark</span>
-            )}
-          </>
-          <Switch
-            checked={theme === "dark"}
-            onChange={toggleTheme}
-            uncheckedIcon={false}
-            checkedIcon={false}
-            height={15}
-            width={30}
-            handleDiameter={11}
-            offColor="#7f8188"
-            onColor="#7f8188"
-            offHandleColor="#cbcbcd"
-            onHandleColor="#1f2027"
-            activeBoxShadow="0 0 0 2px #a8cbf5"
-          />
-        </label>
-      </div>
-      <ul className={styles.history__list}>
-        {history.map(({ val, timestamp }, index) => (
-          <li
-            key={index}
-            className={cx(styles.stage, {
-              [styles["stage--active"]]: activeIndex === index,
-            })}
-          >
-            <div className={styles.stage__header}>
-              <span className={styles.stage__status}>{val.status}</span>
-              <span className={styles.stage__timestamp}>{timestamp}</span>
-              <button
-                className={styles.stage__button}
-                disabled={activeIndex === index}
-                onClick={() => {
-                  setActiveIndex(index);
-                  timeTravel(val);
-                }}
-              >
-                View
-              </button>
-            </div>
-            {renderPayload(val.payload)}
-          </li>
-        ))}
-      </ul>
-    </section>
+    <Rnd
+      default={{
+        x: 0,
+        y: 0,
+        width: 300,
+        height: 400,
+      }}
+      minWidth={300}
+      minHeight={300}
+      dragHandleClassName="js-header"
+    >
+      <section className={cx(styles.history, styles[`history--${theme}`])}>
+        <div className={cx(styles.header, "js-header")}>
+          <p className={styles.header__title}>{sequenceId}</p>
+          <label className={styles.switch}>
+            <>
+              {theme === "dark" && (
+                <span className={styles.switch__label}>Light</span>
+              )}
+              {theme === "light" && (
+                <span className={styles.switch__label}>Dark</span>
+              )}
+            </>
+            <Switch
+              checked={theme === "dark"}
+              onChange={toggleTheme}
+              uncheckedIcon={false}
+              checkedIcon={false}
+              height={15}
+              width={30}
+              handleDiameter={11}
+              offColor="#7f8188"
+              onColor="#7f8188"
+              offHandleColor="#cbcbcd"
+              onHandleColor="#1f2027"
+              activeBoxShadow="0 0 0 2px #a8cbf5"
+            />
+          </label>
+        </div>
+        <ul className={styles.history__list}>
+          {history.map(({ val, timestamp }, index) => (
+            <li
+              key={index}
+              className={cx(styles.stage, {
+                [styles["stage--active"]]: activeIndex === index,
+              })}
+            >
+              <div className={styles.stage__header}>
+                <span className={styles.stage__status}>{val.status}</span>
+                <span className={styles.stage__timestamp}>{timestamp}</span>
+                <button
+                  className={styles.stage__button}
+                  disabled={activeIndex === index}
+                  onClick={() => {
+                    setActiveIndex(index);
+                    timeTravel(val);
+                  }}
+                >
+                  View
+                </button>
+              </div>
+              {renderPayload(val.payload)}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Rnd>
   );
 }
 
