bf55d6c921debda4fcf5d8cd646100e638651f9a
Callum Hart <callum@callumhart.com>
2020-06-04 21:29:14 +0100

    User Journey components

--- src/examples/UserJourney/StockBidder.js
+++ src/examples/UserJourney/StockBidder.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { sequence } from "../../lib";
+import {
+  Button,
+  Form,
+  FormGroup,
+  TextInput,
+  InlineNotification,
+  InlineLoading,
+  Tile,
+} from "carbon-components-react";
 
+import { sequence } from "../../lib";
 import { stockBidSequence } from "./sequence";
 
 export const StockBidder = (props) => {
@@ -11,81 +20,109 @@ export const StockBidder = (props) => {
   ] = props.stockBid;
   const [price, setPrice] = useState("");
   const [units, setUnits] = useState("");
+  const renderForm = () => (
+    <>
+      {status === "FAILED" && (
+        <InlineNotification
+          hideCloseButton={true}
+          kind="error"
+          title={payload.message}
+        />
+      )}
+      <FormGroup>
+        <TextInput
+          id="price"
+          labelText="Bid price (£)"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+      </FormGroup>
+      <FormGroup>
+        <TextInput
+          id="units"
+          labelText="No. of shares"
+          value={units}
+          onChange={(e) => setUnits(e.target.value)}
+        />
+      </FormGroup>
+      <Button disabled={!price || !units} onClick={() => transition("CONFIRM")}>
+        Next
+      </Button>
+    </>
+  );
+  const renderConfirm = () => (
+    <>
+      {status === "CONFIRM" && <p>{payload.timeToBid} seconds left to bid</p>}
+      {status === "SUBMITTING" && (
+        <InlineLoading description="Submitting bid" />
+      )}
+      <Tile>
+        <p>
+          <strong>Bid price</strong> £{price}
+        </p>
+        <p>
+          <strong>No. of shares</strong> {units}
+        </p>
+      </Tile>
+      <div className="bx--btn-set">
+        <Button
+          kind="secondary"
+          onClick={() => {
+            suspend(); // cancels bidding window
+            transition("CAPTURE");
+          }}
+        >
+          Back
+        </Button>
+        <Button
+          type="submit"
+          disabled={status === "SUBMITTING"}
+          onClick={() => {
+            suspend(); // cancels bidding window
+            transition("SUBMIT", { price, units });
+          }}
+        >
+          Confirm
+        </Button>
+      </div>
+    </>
+  );
+  const renderSuccess = () => (
+    <>
+      <InlineNotification
+        hideCloseButton={true}
+        kind="success"
+        title="Bid was successful"
+      />
+      <Button
+        onClick={() => {
+          setPrice("");
+          setUnits("");
+          transition();
+        }}
+      >
+        Place another bid
+      </Button>
+    </>
+  );
 
   useEffect(() => {
     transition();
   }, [transition]);
 
   return (
-    <form>
-      {(status === "CAPTURE" || status === "FAILED") && (
-        <>
-          {status === "FAILED" && <p>{payload.message}</p>}
-          <label htmlFor="price">Price: </label>
-          <input
-            id="price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-          <label htmlFor="units">Units: </label>
-          <input
-            id="units"
-            value={units}
-            onChange={(e) => setUnits(e.target.value)}
-          />
-          <button
-            disabled={!price || !units}
-            onClick={() => transition("CONFIRM")}
-          >
-            Next
-          </button>
-        </>
-      )}
-      {(status === "CONFIRM" || status === "SUBMITTING") && (
-        <>
-          <p>
-            Price: {price} Units: {units}
-          </p>
-          {status === "CONFIRM" && (
-            <p>{payload.timeToBid} seconds left to bid</p>
-          )}
-          <button
-            onClick={() => {
-              suspend(); // cancels bidding window
-              transition("CAPTURE");
-            }}
-          >
-            Back
-          </button>
-          <button
-            type="submit"
-            disabled={status === "SUBMITTING"}
-            onClick={() => {
-              suspend(); // cancels bidding window
-              transition("SUBMIT", { price, units });
-            }}
-          >
-            Confirm
-          </button>
-        </>
-      )}
-      {status === "SUCCESS" && (
-        <>
-          <p>Bid was successful</p>
-          <button
-            onClick={() => {
-              setPrice("");
-              setUnits("");
-              transition();
-            }}
-          >
-            Place another bid
-          </button>
-        </>
-      )}
+    <>
+      <h1>Stock Bidder</h1>
+      <Form action="#" autoComplete="off">
+        {(status === "CAPTURE" || status === "FAILED") && <>{renderForm()}</>}
+        {(status === "CONFIRM" || status === "SUBMITTING") && (
+          <>{renderConfirm()}</>
+        )}
+        {status === "SUCCESS" && <>{renderSuccess()}</>}
 
-      {props.showYsuHistory && <>{history}</>}
-    </form>
+        {props.showYsuHistory && <>{history}</>}
+      </Form>
+    </>
   );
 };
 
